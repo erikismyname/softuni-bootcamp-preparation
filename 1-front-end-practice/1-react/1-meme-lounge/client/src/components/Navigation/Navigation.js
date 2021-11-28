9ff39ee86eb88d480ff2389a4c179c8fe7237c5a
erikivanov7 <advictoriam@abv.bg>
2021-11-28 21:10:11 +0200

    Implemented nav view logic

--- 1-front-end-practice/1-react/1-meme-lounge/client/src/components/Navigation/Navigation.js
+++ 1-front-end-practice/1-react/1-meme-lounge/client/src/components/Navigation/Navigation.js
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom';
 
+import { useUser } from '../../context/UserContext.js';
+
 import UserNavigation from "./UserNavigation/UserNavigation.js";
 import GuestNavigation from "./GuestNavigation/GuestNavigation.js";
 
 const Navigation = () => {
 
+    const { user } = useUser();
+
     return (
         <nav>
             <Link to="/all-memes">All Memes</Link>
-            <UserNavigation />
-            <GuestNavigation />
+
+            {user
+                ? <UserNavigation />
+                : <GuestNavigation />
+            }
+
         </nav>
     );
 
