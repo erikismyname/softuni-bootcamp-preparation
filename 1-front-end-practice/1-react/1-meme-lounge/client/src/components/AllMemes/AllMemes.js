216ceb3b1804cc23a13e304e51871afe639adb56
erikivanov7 <advictoriam@abv.bg>
2021-11-28 21:34:40 +0200

    Implemented all memes page logic

--- 1-front-end-practice/1-react/1-meme-lounge/client/src/components/AllMemes/AllMemes.js
+++ 1-front-end-practice/1-react/1-meme-lounge/client/src/components/AllMemes/AllMemes.js
@@ -1,12 +1,39 @@
+import { useState, useEffect } from 'react';
+
+import { getAllMemes } from '../../services/memeService.js';
+
 import AllMemesCard from "./AllMemesCard/AllMemesCard.js";
 
 const AllMemes = () => {
+
+    const [memes, setMemes] = useState([]);
+
+    useEffect(async () => {
+
+        try {
+
+            const memes = await getAllMemes();
+
+            setMemes(memes);
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    }, []);
 
     return (
         <section id="meme-feed">
             <h1>All Memes</h1>
             <div id="memes">
-                <p className="no-memes">No memes in database.</p>
+
+                {memes.length
+                    ? memes.map(m => <AllMemesCard key={m._id} meme={m} />)
+                    : <p className="no-memes">No memes in database.</p>
+                }
+
             </div>
         </section>
     );
