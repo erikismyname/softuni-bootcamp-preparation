f1a31ed69331f3a135d95667b198edfa2b519338
erikivanov7 <advictoriam@abv.bg>
2021-11-29 16:26:17 +0200

    Implemented details page functionality

--- 1-front-end-practice/1-react/1-meme-lounge/client/src/components/DetailsMeme/DetailsMeme.js
+++ 1-front-end-practice/1-react/1-meme-lounge/client/src/components/DetailsMeme/DetailsMeme.js
@@ -1,21 +1,60 @@
-const DetailsMeme = () => {
+import { useState, useEffect } from 'react';
+
+import { useUser } from '../../context/UserContext.js';
+
+import { getOneMeme, deleteMeme } from '../../services/memeService.js';
+
+import DetailsMemeBtns from './DetailsMemeBtns/DetailsMemeBtns.js';
+
+const DetailsMeme = ({ history, match }) => {
+
+    const memeId = match.params.memeId;
+
+    const { user } = useUser();
+
+    const [meme, setMeme] = useState([]);
+
+    useEffect(async () => {
+
+        try {
+
+            const meme = await getOneMeme(memeId);
+
+            setMeme(meme);
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    }, []);
 
     return (
         <section id="meme-details">
-            <h1>Meme Title: Bad code can present some problems</h1>
+            <h1>Meme Title: {meme.title}</h1>
             <div className="meme-details">
                 <div className="meme-img">
-                    <img alt="meme-alt" src="/images/3.png" />
+                    <img alt="meme-alt" src={meme.imageUrl} />
                 </div>
                 <div className="meme-description">
                     <h2>Meme Description</h2>
                     <p>
-                        Being a programmer is a fun job. And many funny incidents occur throughout a
-                        programmer’s career.
-                        Here are a few jokes that can be relatable to you as a programmer.
+                        {meme.description}
                     </p>
-                    <a className="button warning" href="#">Edit</a>
-                    <button className="button danger">Delete</button>
+
+                    {
+                        meme._ownerId == user._id
+                            ?
+                            <DetailsMemeBtns
+                                token={user.accessToken}
+                                memeId={meme._id}
+                                deleteMeme={deleteMeme}
+                                history={history}
+                            />
+                            : ''
+                    }
+
                 </div>
             </div>
         </section>
