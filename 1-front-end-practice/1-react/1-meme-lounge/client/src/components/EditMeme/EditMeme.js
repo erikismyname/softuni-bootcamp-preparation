76681a74c5971a401643a8110cda4cef34289da5
erikivanov7 <advictoriam@abv.bg>
2021-11-29 16:52:34 +0200

    Implemented edit page functionality

--- 1-front-end-practice/1-react/1-meme-lounge/client/src/components/EditMeme/EditMeme.js
+++ 1-front-end-practice/1-react/1-meme-lounge/client/src/components/EditMeme/EditMeme.js
@@ -1,23 +1,72 @@
-const EditMeme = () => {
+import { useState, useEffect } from 'react';
+
+import { useUser } from '../../context/UserContext.js';
+
+import { getOneMeme, editMeme } from '../../services/memeService.js';
+
+const EditMeme = ({ history, match }) => {
+
+    const memeId = match.params.memeId;
+
+    const { user } = useUser();
+
+    const [meme, setMeme] = useState([]);
+
+    useEffect(async () => {
+
+        try {
+
+            const meme = await getOneMeme(memeId);
+
+            setMeme(meme);
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    }, []);
+
+    const onFormSubmit = async (ev) => {
+
+        ev.preventDefault();
+
+        const formData = new FormData(ev.target);
+
+        const title = formData.get('title').trim();
+
+        const description = formData.get('description').trim();
+
+        const imageUrl = formData.get('imageUrl').trim();
+
+        if (!title || !description || !imageUrl) return alert('All fields are required!');
+
+        try {
+
+            await editMeme(memeId, { title, description, imageUrl }, user.accessToken);
+
+            history.push(`/details/${memeId}`);
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    }
 
     return (
         <section id="edit-meme">
-            <form id="edit-form">
+            <form id="edit-form" onSubmit={onFormSubmit}>
                 <h1>Edit Meme</h1>
                 <div className="container">
                     <label htmlFor="title">Title</label>
-                    <input id="title" type="text" placeholder="Enter Title" name="title" />
+                    <input id="title" type="text" placeholder="Enter Title" name="title" defaultValue={meme.title} />
                     <label htmlFor="description">Description</label>
-                    <textarea id="description" placeholder="Enter Description" name="description">
-                        Programming is often touted as a smart and lucrative career path.
-                        It's a job that (sometimes) offers flexibility and great benefits.
-                        But it's far from sunshine and Nyan Cat rainbows. The hours are long.
-                        The mistakes are frustrating. And your eyesight is almost guaranteed to suffer.
-                        These memes cover most of the frustration (and funny moments) of programming.
-                        At least we can laugh through the pain.
-                    </textarea>
+                    <textarea id="description" placeholder="Enter Description" name="description" defaultValue={meme.description}></textarea>
                     <label htmlFor="imageUrl">Image Url</label>
-                    <input id="imageUrl" type="text" placeholder="Enter Meme ImageUrl" name="imageUrl" />
+                    <input id="imageUrl" type="text" placeholder="Enter Meme ImageUrl" name="imageUrl" defaultValue={meme.imageUrl} />
                     <input type="submit" className="registerbtn button" value="Edit Meme" />
                 </div>
             </form>
