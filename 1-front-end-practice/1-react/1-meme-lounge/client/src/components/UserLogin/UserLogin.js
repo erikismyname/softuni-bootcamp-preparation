3686f9ebe8a02ae2baaf70c53f9193fa6fe20acf
erikivanov7 <advictoriam@abv.bg>
2021-11-29 14:04:29 +0200

    Implemented login page functionality

--- 1-front-end-practice/1-react/1-meme-lounge/client/src/components/UserLogin/UserLogin.js
+++ 1-front-end-practice/1-react/1-meme-lounge/client/src/components/UserLogin/UserLogin.js
@@ -1,8 +1,44 @@
-const UserLogin = () => {
+import { Link } from 'react-router-dom';
+
+import { useUser } from '../../context/UserContext.js';
+
+import { loginUser } from '../../services/userService.js';
+
+const UserLogin = ({ history }) => {
+
+    const { addUser } = useUser();
+
+    const onFormSubmit = async (ev) => {
+
+        ev.preventDefault();
+
+        const formData = new FormData(ev.target);
+
+        const email = formData.get('email').trim();
+
+        const password = formData.get('password').trim();
+
+        if (!email || !password) return alert('Both fields are required!');
+
+        try {
+
+            const user = await loginUser({ email, password });
+
+            addUser(user);
+
+            history.push('all-memes');
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    };
 
     return (
         <section id="login">
-            <form id="login-form">
+            <form onSubmit={onFormSubmit} id="login-form">
                 <div className="container">
                     <h1>Login</h1>
                     <label htmlFor="email">Email</label>
@@ -11,7 +47,7 @@ const UserLogin = () => {
                     <input id="password" type="password" placeholder="Enter Password" name="password" />
                     <input type="submit" className="registerbtn button" value="Login" />
                     <div className="container signin">
-                        <p>Dont have an account?<a href="#">Sign up</a>.</p>
+                        <p>Dont have an account?<Link to="/register">Sign up</Link>.</p>
                     </div>
                 </div>
             </form>
