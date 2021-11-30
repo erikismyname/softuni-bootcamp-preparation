35667f6d90b07c598bb9809a18b8bade73f84652
erikivanov7 <advictoriam@abv.bg>
2021-11-30 23:22:03 +0200

    Wrapped jsx around return statement in register

--- 1-front-end-practice/1-react/2-games-play/client/src/components/Register/Register.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Register/Register.js
@@ -1,28 +1,32 @@
 const Register = () => {
 
-    <section id="register-page" class="content auth">
-        <form id="register">
-            <div class="container">
-                <div class="brand-logo"></div>
-                <h1>Register</h1>
+    return (
 
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="maria@email.com" />
+        <section id="register-page" class="content auth">
+            <form id="register">
+                <div class="container">
+                    <div class="brand-logo"></div>
+                    <h1>Register</h1>
 
-                <label htmlFor="pass">Password:</label>
-                <input type="password" name="password" id="register-password" />
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="maria@email.com" />
 
-                <label htmlFor="con-pass">Confirm Password:</label>
-                <input type="password" name="confirm-password" id="confirm-password" />
+                    <label htmlFor="pass">Password:</label>
+                    <input type="password" name="password" id="register-password" />
 
-                <input class="btn submit" type="submit" value="Register" />
+                    <label htmlFor="con-pass">Confirm Password:</label>
+                    <input type="password" name="confirm-password" id="confirm-password" />
 
-                <p class="field">
-                    <span>If you already have profile click <a href="#">here</a></span>
-                </p>
-            </div>
-        </form>
-    </section>
+                    <input class="btn submit" type="submit" value="Register" />
+
+                    <p class="field">
+                        <span>If you already have profile click <a href="#">here</a></span>
+                    </p>
+                </div>
+            </form>
+        </section>
+
+    );
 
 };
 
