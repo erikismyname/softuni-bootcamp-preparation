36ed51ff74fd9d372c6dc0695d507d14336d0b1d
erikivanov7 <advictoriam@abv.bg>
2021-11-30 22:56:35 +0200

    Created game header comp

---
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Details/InfoSection/GameHeader/GameHeader.js
@@ -0,0 +1,16 @@
+const GameHeader = () => {
+
+    retrun(
+
+        <div class="game-header">
+            <img class="game-img" src="images/MineCraft.png" />
+            <h1>Bright</h1>
+            <span class="levels">MaxLevel: 4</span>
+            <p class="type">Action, Crime, Fantasy</p>
+        </div>
+
+    );
+
+};
+
+export default GameHeader;
