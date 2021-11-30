a3e3edb5fc0bf573bdaf1ac5357da9ec7a41e394
erikivanov7 <advictoriam@abv.bg>
2021-11-30 22:54:26 +0200

    Created a catlog game card comp

---
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Catalog/CatalogGameCard/CatalogGameCard.js
@@ -0,0 +1,18 @@
+const CatalogGameCard = () => {
+
+    return (
+
+        <div class="allGames">
+            <div class="allGames-info">
+                <img src="./images/avatar-1.jpg" />
+                <h6>Action</h6>
+                <h2>Cover Fire</h2>
+                <a href="#" class="details-button">Details</a>
+            </div>
+        </div>
+
+    );
+
+};
+
+export default CatalogGameCard;
