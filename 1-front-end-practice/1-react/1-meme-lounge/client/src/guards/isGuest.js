a37ba5b0ebf984f6a71c4dec2f4db8f00a2265ee
erikivanov7 <advictoriam@abv.bg>
2021-11-30 18:57:55 +0200

    Removed unnecessary console.log()

--- 1-front-end-practice/1-react/1-meme-lounge/client/src/guards/isGuest.js
+++ 1-front-end-practice/1-react/1-meme-lounge/client/src/guards/isGuest.js
@@ -8,8 +8,6 @@ const isGuest = (WrappedComponent) => {
 
         const { user } = useUser();
 
-        console.log(user);
-
         if (Object.keys(user).length) return <Redirect to="/all-memes" />;
 
         return <WrappedComponent {...props} />
