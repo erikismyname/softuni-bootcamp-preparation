9ee1de58fe1a962b0c028874a66f53734d81283c
erikivanov7 <advictoriam@abv.bg>
2021-11-30 22:56:17 +0200

    Created game comments comp

---
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Details/InfoSection/GameComments/GameComments.js
@@ -0,0 +1,22 @@
+const GameComments = () => {
+
+    return (
+
+        <div class="details-comments">
+            <h2>Comments:</h2>
+            <ul>
+                <li class="comment">
+                    <p>Content: I rate this one quite highly.</p>
+                </li>
+                <li class="comment">
+                    <p>Content: The best game.</p>
+                </li>
+            </ul>
+            <p class="no-comment">No comments.</p>
+        </div>
+
+    );
+
+};
+
+export default GameComments;     
