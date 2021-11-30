25f1d7f1dc35662487f44939e700b457a0976475
erikivanov7 <advictoriam@abv.bg>
2021-11-30 22:56:56 +0200

    Imported all comp in the app comp

--- 1-front-end-practice/1-react/2-games-play/client/src/App.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/App.js
@@ -1,6 +1,38 @@
+import Header from "./components/Header/Header.js";
+import Home from "./components/Home/Home.js";
+import Login from "./components/Login/Login.js";
+import Register from "./components/Register/Register.js";
+import Create from "./components/Create/Create.js";
+import Edit from "./components/Edit/Edit.js";
+import Details from "./components/Details/Details.js";
+import Catalog from "./components/Catalog/Catalog.js";
+
 function App() {
 
-    return null;
+    return (
+
+        <div id="box">
+
+            <Header />
+
+            <Home />
+
+            <Login />
+
+            <Register />
+
+            <Create />
+
+            <Edit />
+
+            <Details />
+
+            <Catalog />
+
+        </div >
+
+
+    );
 
 }
 
