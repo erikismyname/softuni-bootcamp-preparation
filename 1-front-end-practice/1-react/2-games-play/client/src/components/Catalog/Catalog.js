af64e15fae107b078fff1b8a6db0bba3ae101cf0
erikivanov7 <advictoriam@abv.bg>
2021-11-30 22:50:12 +0200

    Updated problem resources

---
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Catalog/Catalog.js
@@ -0,0 +1,19 @@
+import CatalogGameCard from "./CatalogGameCard/CatalogGameCard.js";
+
+const Catalog = () => {
+
+    return (
+
+        <section id="catalog-page">
+            <h1>All Games</h1>
+            
+            <CatalogGameCard />
+
+            <h3 class="no-articles">No articles yet</h3>
+        </section>
+
+    );
+
+};
+
+export default Catalog;
