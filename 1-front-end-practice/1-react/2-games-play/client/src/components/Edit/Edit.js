5222b19e345e31fce5ce127cbed5e3507703e789
erikivanov7 <advictoriam@abv.bg>
2021-11-30 22:54:09 +0200

    Created an edit comp

---
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Edit/Edit.js
@@ -0,0 +1,34 @@
+const Edit = () => {
+
+    return (
+
+        <section id="edit-page" class="auth">
+            <form id="edit">
+                <div class="container">
+
+                    <h1>Edit Game</h1>
+                    <label htmlFor="leg-title">Legendary title:</label>
+                    <input type="text" id="title" name="title" value="" />
+
+                    <label htmlFor="category">Category:</label>
+                    <input type="text" id="category" name="category" value="" />
+
+                    <label htmlFor="levels">MaxLevel:</label>
+                    <input type="number" id="maxLevel" name="maxLevel" min="1" value="" />
+
+                    <label htmlFor="game-img">Image:</label>
+                    <input type="text" id="imageUrl" name="imageUrl" value="" />
+
+                    <label htmlFor="summary">Summary:</label>
+                    <textarea name="summary" id="summary"></textarea>
+                    <input class="btn submit" type="submit" value="Edit Game" />
+
+                </div>
+            </form>
+        </section>
+
+    );
+
+};
+
+export default Edit;
