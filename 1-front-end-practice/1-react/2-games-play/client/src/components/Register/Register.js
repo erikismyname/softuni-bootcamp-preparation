2c7b691d12698c8fbd9c5170566c8d34d7ef5b66
erikivanov7 <advictoriam@abv.bg>
2021-12-01 19:37:57 +0200

    Implemented register functionality

--- 1-front-end-practice/1-react/2-games-play/client/src/components/Register/Register.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Register/Register.js
@@ -1,18 +1,56 @@
+import { Link } from "react-router-dom";
+
+import useUser from "../../hooks/useUser.js";
+import { registerUser } from '../../services/userService.js';
 import Input from "../common/forms/Input/Input.js";
 import Label from "../common/forms/Label/Label.js";
 
-const Register = () => {
+const Register = ({ history }) => {
+
+    const { addUser } = useUser();
+
+    const onRegisterFormSubmit = async (ev) => {
+
+        ev.preventDefault();
+
+        const [email, password, rePass] = [...ev.target].slice(0, 3).map(i => i.value);
+
+        if (!email || !password || !rePass) {
+
+            return alert('All fields are required!');
+
+        } else if (password != rePass) {
+
+            return alert('Passwords must match!');
+
+        }
+
+        try {
+
+            const user = await registerUser({ email, password });
+
+            addUser(user);
+
+            history.push('/');
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    };
 
     return (
 
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" onSubmit={onRegisterFormSubmit}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
 
                     <Label htmlFor="email" text="Email:" />
-                    <Input type="email" id="email" name="email" placeholder="maria@email.com" />
+                    <Input type="text" id="email" name="email" placeholder="maria@email.com" />
 
                     <Label htmlFor="pass" text="Password:" />
                     <Input type="password" name="password" id="register-password" />
@@ -23,7 +61,7 @@ const Register = () => {
                     <Input className="btn submit" type="submit" value="Register" />
 
                     <p className="field">
-                        <span>If you already have profile click <a href="#">here</a></span>
+                        <span>If you already have profile click <Link to="/login">here</Link></span>
                     </p>
                 </div>
             </form>
