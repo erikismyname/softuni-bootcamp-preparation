74274fb42762b004bbf69b47da16a5b43ca1d2df
erikivanov7 <advictoriam@abv.bg>
2021-12-01 19:49:57 +0200

    Implemented catalog page functionality

--- 1-front-end-practice/1-react/2-games-play/client/src/components/Catalog/Catalog.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Catalog/Catalog.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react';
+
+import { getAllGames } from '../../services/gameService.js';
 import CatalogGameCard from "./CatalogGameCard/CatalogGameCard.js";
 
 const Catalog = () => {
+
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+
+        getAllGames()
+            .then(games => setGames(games))
+            .catch(err => alert(err));
+
+    }, []);
 
     return (
 
         <section id="catalog-page">
             <h1>All Games</h1>
-            <CatalogGameCard />
-            <h3 className="no-articles">No articles yet</h3>
+
+            {games.length
+                ? games.map(g => <CatalogGameCard key={g._id} game={g} />)
+                : <h3 className="no-articles">No articles yet</h3>
+            }
+
         </section>
 
     );
--- 1-front-end-practice/1-react/2-games-play/client/src/components/Catalog/CatalogGameCard/CatalogGameCard.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Catalog/CatalogGameCard/CatalogGameCard.js
@@ -1,13 +1,15 @@
-const CatalogGameCard = () => {
+import { Link } from 'react-router-dom';
+
+const CatalogGameCard = ({ game }) => {
 
     return (
 
         <div className="allGames">
             <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>Cover Fire</h2>
-                <a href="#" className="details-button">Details</a>
+                <img src={game.imageUrl} />
+                <h6>{game.category}</h6>
+                <h2>{game.title}</h2>
+                <Link to={`/details/${game._id}`} className="details-button">Details</Link>
             </div>
         </div>
 
