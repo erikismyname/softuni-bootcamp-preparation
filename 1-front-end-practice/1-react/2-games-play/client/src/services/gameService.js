3afb56d4dc230f97a4ff8e2877d72abbbd360827
erikivanov7 <advictoriam@abv.bg>
2021-12-01 15:30:05 +0200

    Implemented game service

---
+++ 1-front-end-practice/1-react/2-games-play/client/src/services/gameService.js
@@ -0,0 +1,47 @@
+import { ENDPOINTS } from '../config/constants.js';
+import * as api from '../api/api.js';
+
+async function getAllGames() {
+
+    return api.getRequest(`${ENDPOINTS.game}?sortBy=_createdOn%20desc`);
+
+}
+
+async function getRecentGames() {
+
+    return api.getRequest(`${ENDPOINTS.game}?sortBy=_createdOn%20desc&distinct=category`);
+
+}
+
+async function getOneGame(gameId) {
+
+    return api.getRequest(`${ENDPOINTS.game}/${gameId}`);
+
+}
+
+async function createGame(token, gameData) {
+
+    return api.postRequest(ENDPOINTS.game, token, gameData);
+
+}
+
+async function editGame(gameId, token, gameData) {
+
+    return api.putRequest(`${ENDPOINTS.game}/${gameId}`, token, gameData);
+
+}
+
+async function deleteGame(gameId, token) {
+
+    return api.deleteRequest(`${ENDPOINTS.game}/${gameId}`, token);
+
+}
+
+export {
+    getAllGames,
+    getRecentGames,
+    getOneGame,
+    createGame,
+    editGame,
+    deleteGame,
+};
