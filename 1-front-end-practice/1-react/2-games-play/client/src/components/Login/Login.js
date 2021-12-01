e748661880114be8e9eb678940e0d480c4bbe93b
erikivanov7 <advictoriam@abv.bg>
2021-12-01 19:28:00 +0200

    Implemented login functionality

--- 1-front-end-practice/1-react/2-games-play/client/src/components/Login/Login.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Login/Login.js
@@ -1,27 +1,59 @@
+import { Link } from 'react-router-dom';
+
+import useUser from '../../hooks/useUser.js';
+import { loginUser } from '../../services/userService.js';
 import Input from "../common/forms/Input/Input.js";
 import Label from "../common/forms/Label/Label.js";
 
-const Login = () => {
+const Login = ({ history }) => {
+
+    const { addUser } = useUser();
+
+    const onFormSubmit = async (ev) => {
+
+        ev.preventDefault();
+
+        console.log();
+
+        const [email, password] = [...ev.target].slice(0, 2).map(i => i.value);
+
+        if (!email || !password) return alert('All fields are required!');
+
+        try {
+
+            const user = await loginUser({ email, password });
+
+            addUser(user);
+
+            history.push('/');
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    };
 
     return (
 
-        <section id="login-page" class="auth">
-            <form id="login">
+        <section id="login-page" className="auth">
+            <form id="login" onSubmit={onFormSubmit}>
 
-                <div class="container">
-                    <div class="brand-logo"></div>
+                <div className="container">
+                    <div className="brand-logo"></div>
                     <h1>Login</h1>
 
                     <Label for="email" text="Email:" />
-                    <Input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <Input type="text" id="email" name="email" placeholder="Sokka@gmail.com" />
 
                     <Label for="login-pass" text="Password:" />
                     <Input type="password" id="login-password" name="password" />
 
-                    <Input type="submit" class="btn submit" value="Login" />
+                    <Input type="submit" className="btn submit" value="Login" />
 
-                    <p class="field" >
-                        <span>If you don't have profile click <a href="#">here</a></span>
+                    <p className="field" >
+                        <span>If you don't have profile click <Link to="/register">here</Link></span>
                     </p>
                 </div>
             </form>
