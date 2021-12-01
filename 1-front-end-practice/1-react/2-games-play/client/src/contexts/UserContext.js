ea69c75ff7acb3807e455a7e7b5e6c9b9c5f4201
erikivanov7 <advictoriam@abv.bg>
2021-12-01 15:54:22 +0200

    Created a ctx comp

---
+++ 1-front-end-practice/1-react/2-games-play/client/src/contexts/UserContext.js
@@ -0,0 +1,26 @@
+import { createContext, useState } from 'react';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+
+    const [user, setUser] = useState({});
+
+    const addUser = (user) => setUser(user);
+
+    const value = { user, addUser };
+
+    return (
+
+        <UserContext.Provider value={value}>
+            {children}
+        </UserContext.Provider>
+
+    );
+
+};
+
+export {
+    UserContext,
+    UserProvider,
+};
