1c6fd03c1c6bcf05b1984825b919850aa4018840
erikivanov7 <advictoriam@abv.bg>
2021-12-02 15:31:54 +0200

    Implemented edit page functionality

--- 1-front-end-practice/1-react/2-games-play/client/src/components/Edit/Edit.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Edit/Edit.js
@@ -1,32 +1,76 @@
+import { useState, useEffect } from 'react';
+
+import useUser from '../../hooks/useUser.js';
+import { getOneGame, editGame } from '../../services/gameService.js';
 import Label from "../common/forms/Label/Label.js";
 import Input from "../common/forms/Input/Input.js";
 import Textarea from "../common/forms/Textarea/Textarea.js";
 
-const Edit = () => {
+const Edit = ({ match, history }) => {
+
+    const gameId = match.params.gameId;
+
+    const { user } = useUser();
+
+    const [game, setGame] = useState({});
+
+    useEffect(() => {
+
+        getOneGame(gameId)
+            .then(game => setGame(game))
+            .catch(err => alert(err));
+
+    }, []);
+
+    const onEditFormSubmit = async (ev) => {
+
+        ev.preventDefault();
+
+        const [title, category, maxLevel, imageUrl, summary] = [...ev.target].slice(0, 5).map(i => i.value).map(v => v.trim());
+
+        if (!title || !category || !maxLevel || !imageUrl || !summary) {
+
+            return alert('All fields are required!');
+
+        }
+
+        try {
+
+            await editGame(gameId, user.accessToken, { title, category, maxLevel, imageUrl, summary });
+
+            history.push(`/details/${gameId}`)
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    };
 
     return (
 
         <section id="edit-page" className="auth">
-            <form id="edit">
+            <form id="edit" onSubmit={onEditFormSubmit}>
                 <div className="container">
                     <h1>Edit Game</h1>
 
                     <Label htmlFor="leg-title" text="Legendary title:" />
-                    <Input type="text" id="title" name="title" defaultValue="" />
+                    <Input type="text" id="title" name="title" defaultValue={game.title} />
 
                     <Label htmlFor="category" text="Category:" />
-                    <Input type="text" id="category" name="category" defaultValue="" />
+                    <Input type="text" id="category" name="category" defaultValue={game.category} />
 
                     <Label htmlFor="levels" text="MaxLevel:" />
-                    <Input type="number" id="maxLevel" name="maxLevel" min="1" defaultValue="" />
+                    <Input type="number" id="maxLevel" name="maxLevel" min="1" defaultValue={game.maxLevel} />
 
                     <Label htmlFor="game-img" text="Image:" />
-                    <Input type="text" id="imageUrl" name="imageUrl" defaultValue="" />
+                    <Input type="text" id="imageUrl" name="imageUrl" defaultValue={game.imageUrl} />
 
                     <Label htmlFor="summary" text="Summary:" />
-                    <Textarea name="summary" id="summary" />
+                    <Textarea name="summary" id="summary" defaultValue={game.summary} />
 
-                    <Input className="btn submit" type="submit" value="Edit Game" />
+                    <input className="btn submit" type="submit" value="Edit Game" />
 
                 </div>
             </form>
