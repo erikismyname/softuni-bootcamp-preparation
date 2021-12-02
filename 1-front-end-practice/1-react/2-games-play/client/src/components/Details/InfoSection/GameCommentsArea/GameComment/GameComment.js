51a27c92bac064f8d6c6d86b7f135d63328f8199
erikivanov7 <advictoriam@abv.bg>
2021-12-02 10:44:37 +0200

    Added a game comment component

---
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Details/InfoSection/GameCommentsArea/GameComment/GameComment.js
@@ -0,0 +1,13 @@
+const GameComment = ({comment}) => {
+
+    return (
+
+        <li className="comment">
+            <p>Content: {comment}.</p>
+        </li>
+
+    );
+
+};
+
+export default GameComment;
