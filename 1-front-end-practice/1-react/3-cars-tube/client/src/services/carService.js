b97ecda5881933be4873cb33dac44bb0002b9ad2
erikivanov7 <advictoriam@abv.bg>
2021-12-02 21:31:03 +0200

    Added car service

---
+++ 1-front-end-practice/1-react/3-cars-tube/client/src/services/carService.js
@@ -0,0 +1,18 @@
+import { ENDPOINTS } from '../config/constants.js';
+import * as api from '../api/api.js';
+
+const getAllCars = async () => api.getRequest(`${ENDPOINTS.car}/?sortBy=_createdOn%20desc`);
+
+const getOneCar = async (carId) => api.getRequest(`${ENDPOINTS.car}/${carId}`);
+
+const getMyCars = async (userId, token) => api.getRequest(`${ENDPOINTS.car}?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`, token)
+
+const createCar = async (token, carData) => api.postRequest(ENDPOINTS.car, token, carData);
+
+const updateCar = async (carId, token, carData) => api.putRequest(`${ENDPOINTS.car}/${carId}`, token, carData);
+
+const deleteCar = async (carId, token) => api.deleteRequest(`${ENDPOINTS.car}/${carId}`, token);
+
+const filterCarsByYear = async (carYear, token) => api.getRequest(`${ENDPOINTS.car}/?where=year%3D${carYear}`);
+
+export { getAllCars, getOneCar, getMyCars, createCar, updateCar, deleteCar, filterCarsByYear };
