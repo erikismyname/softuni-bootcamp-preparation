ccd42a9a451a7b1c48958a4a1d6d619a10d56d80
erikivanov7 <advictoriam@abv.bg>
2021-12-02 10:44:18 +0200

    Added comments actions url

--- 1-front-end-practice/1-react/2-games-play/client/src/config/constants.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/config/constants.js
@@ -3,4 +3,5 @@ const BASE_URL = 'http://localhost:3030';
 export const ENDPOINTS = {
     user: `${BASE_URL}/users`,
     game: `${BASE_URL}/data/games`,
+    comment: `${BASE_URL}/data/comments`,
 };
