30ce8a8656f6b34d16c36f9dc9fb6fead2c12a1b
erikivanov7 <advictoriam@abv.bg>
2021-12-02 21:55:46 +0200

    Created user ctx

---
+++ 1-front-end-practice/1-react/3-cars-tube/client/src/contexts/UserContext.js
@@ -0,0 +1,21 @@
+import { createContext, useState } from 'react';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+
+    const addUser = (user) => setUser(user);
+
+    const value = { user, addUser };
+
+    return (
+        <UserContext.Provider value={value}>
+            {children}
+        </UserContext.Provider>
+    );
+
+};
+
+return { UserContext, UserProvider };
