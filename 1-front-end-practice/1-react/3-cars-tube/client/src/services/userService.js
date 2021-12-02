8c4e844922946f885098a9d480d056d016517f66
erikivanov7 <advictoriam@abv.bg>
2021-12-02 21:30:57 +0200

    Fixed url in user service

--- 1-front-end-practice/1-react/3-cars-tube/client/src/services/userService.js
+++ 1-front-end-practice/1-react/3-cars-tube/client/src/services/userService.js
@@ -1,10 +1,10 @@
 import { ENDPOINTS } from '../config/constants.js';
 import { getRequest, postRequest } from '../api/api.js';
 
-const loginUser = async (userData) => postRequest(`${ENDPOINTS}/login`, null, userData);
+const loginUser = async (userData) => postRequest(`${ENDPOINTS.user}/login`, null, userData);
 
-const logoutUser = async (token) => getRequest(`${ENDPOINTS}/logout`, token);
+const logoutUser = async (token) => getRequest(`${ENDPOINTS.user}/logout`, token);
 
-const registerUser = async (userData) => postRequest(`${ENDPOINTS}/register`, null, userData);
+const registerUser = async (userData) => postRequest(`${ENDPOINTS.user}/register`, null, userData);
 
 export { loginUser, logoutUser, registerUser };
