f20ccfa7686869525cb6995f5192f3fc6f11265b
erikivanov7 <advictoriam@abv.bg>
2021-12-02 15:32:33 +0200

    Fixed a typo

--- 1-front-end-practice/1-react/2-games-play/client/src/components/Details/InfoSection/GameHeader/GameHeader.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Details/InfoSection/GameHeader/GameHeader.js
@@ -5,7 +5,7 @@ const GameHeader = ({game}) => {
         <div className="game-header">
             <img className="game-img" alt="" src={game.imageUrl}/>
             <h1>{game.title}</h1>
-            <span className="levels">MaxLevel: {game.maxLvl}</span>
+            <span className="levels">MaxLevel: {game.maxLevel}</span>
             <p className="type">{game.category}</p>
         </div>
 
