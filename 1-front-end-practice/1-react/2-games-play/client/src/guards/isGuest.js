f40db21a6b86a5dc8f47d7bf9fbf5e4820f59078
erikivanov7 <advictoriam@abv.bg>
2021-12-02 16:30:16 +0200

    Created a guest guard

---
+++ 1-front-end-practice/1-react/2-games-play/client/src/guards/isGuest.js
@@ -0,0 +1,22 @@
+import { Redirect } from 'react-router';
+
+import useUser from '../hooks/useUser.js';
+
+const isGuest = (WrappedComponent) => {
+
+    const ComponentWrapper = (props) => {
+
+        const { user } = useUser();
+
+        if (user?.email) return <Redirect to="/" />
+
+        return <WrappedComponent {...props} />
+
+    };
+
+    return ComponentWrapper;
+
+
+};
+
+export default isGuest;
