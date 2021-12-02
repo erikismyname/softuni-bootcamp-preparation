a224fb173441b3573d9dfd50c9bacfdbc2cb1a8c
erikivanov7 <advictoriam@abv.bg>
2021-12-02 15:32:11 +0200

    Implemented logout functionality

--- 1-front-end-practice/1-react/2-games-play/client/src/components/Details/InfoSection/ActionBtns/ActionBtns.js
+++ 1-front-end-practice/1-react/2-games-play/client/src/components/Details/InfoSection/ActionBtns/ActionBtns.js
@@ -1,12 +1,39 @@
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 
-const ActionBtns = ({gameId}) => {
+import { Link } from 'react-router-dom';
+import { deleteGame } from '../../../../services/gameService.js';
+
+const ActionBtns = ({ gameId, user }) => {
+
+    const history = useHistory();
+
+    const onDeleteBtnClick = async () => {
+
+        const isConfirmed = window.confirm('Are you sure?');
+
+        if (isConfirmed) {
+
+            try {
+
+                await deleteGame(gameId, user.accessToken);
+
+                history.push('/');
+
+            } catch (err) {
+
+                alert(err);
+
+            }
+
+        }
+
+    };
 
     return (
 
         <div className="buttons">
             <Link to={`/edit/${gameId}`} className="button">Edit</Link>
-            <button className="button">Delete</button>
+            <button className="button" onClick={onDeleteBtnClick}>Delete</button>
         </div>
 
     );
