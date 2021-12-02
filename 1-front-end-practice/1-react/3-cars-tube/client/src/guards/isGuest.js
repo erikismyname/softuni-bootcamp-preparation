5f8351c51525f94606e3282044fa55f614512a65
erikivanov7 <advictoriam@abv.bg>
2021-12-02 22:14:40 +0200

    Created a guest guard

---
+++ 1-front-end-practice/1-react/3-cars-tube/client/src/guards/isGuest.js
@@ -0,0 +1,21 @@
+import { Redirect } from 'react-router';
+
+import useUser from '../hooks/useUser.js';
+
+const isGuest = (WrappedComponent) => {
+
+    const WrapperComponent = (props) => {
+
+        const { user } = useUser();
+
+        if (user) return <Redirect to="/catalog" />
+
+        return <WrappedComponent {...props} />
+
+    };
+
+    return WrapperComponent;
+
+};
+
+export default isGuest;
