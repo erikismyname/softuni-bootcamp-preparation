6cf1251d24e9a036e073dc9fb36de64af45dfd84
erikivanov7 <advictoriam@abv.bg>
2021-12-03 13:48:26 +0200

    Implemented create functionality

--- 1-front-end-practice/1-react/3-cars-tube/client/src/components/Create/Create.js
+++ 1-front-end-practice/1-react/3-cars-tube/client/src/components/Create/Create.js
@@ -1,11 +1,53 @@
-const Create = () => {
+import useUser from '../../hooks/useUser.js';
+
+import { createCar } from '../../services/carService.js';
+
+const Create = ({ history }) => {
+
+    const { user } = useUser();
+
+    const onCreateFormSubmit = async (ev) => {
+
+        ev.preventDefault();
+
+        const [brand, model, description, year, imageUrl, price] = [...ev.target].slice(0, -1).map(i => i.value).map(e => e.trim());
+
+        if (!brand || !model || !description || !year || !imageUrl || !price) {
+
+            return alert('All fields are required!');
+
+        } else if (year <= 0) {
+
+            return alert('Year must be a positive number!');
+
+        } else if (price <= 0) {
+
+            return alert('Price must be a positive number!');
+
+        }
+
+        const carData = { brand, model, description, year, imageUrl, price };
+
+        try {
+
+            await createCar(user.accessToken, carData);
+
+            history.push('/catalog');
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    };
 
     return (
         <section id="create-listing">
             <div className="container">
-                <form id="create-form">
+                <form id="create-form" onSubmit={onCreateFormSubmit}>
                     <h1>Create Car Listing</h1>
-                    <p>Please fill in this form to create an listing.</p>
+                    <p>Please fill in this form to create a listing.</p>
                     <hr />
 
                     <p>Car Brand</p>
