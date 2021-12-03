3bbbc86bf3db7673bd948079429d786f1c1be478
erikivanov7 <advictoriam@abv.bg>
2021-12-03 15:38:17 +0200

    Created a common car card comp

--- 1-front-end-practice/1-react/3-cars-tube/client/src/components/Catalog/Catalog.js
+++ 1-front-end-practice/1-react/3-cars-tube/client/src/components/Catalog/Catalog.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 import { getAllCars } from '../../services/carService.js';
 
-import CarCard from './CarCard/CarCard.js';
+import CarCard from '../common/CarCard/CarCard.js';
 
 const Catalog = () => {
 
