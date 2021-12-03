fe58461d04dbedd6c3b38badb8571cdf8cdb3d25
erikivanov7 <advictoriam@abv.bg>
2021-12-03 12:30:24 +0200

    Added home page functionality

--- 1-front-end-practice/1-react/3-cars-tube/client/src/components/Home/Home.js
+++ 1-front-end-practice/1-react/3-cars-tube/client/src/components/Home/Home.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
 
     return (
@@ -7,7 +9,7 @@ const Home = () => {
                 <img className="hero" src="/images/car-png.webp" alt="carIntro" />
                 <h2>To see all the listings click the link below:</h2>
                 <div>
-                    <a href="#" className="button">Listings</a>
+                    <Link to="/catalog" className="button">Listings</Link>
                 </div>
             </div>
         </section>
