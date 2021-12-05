eb84c11bd9c6fe72282660585ddc31d5987c4f47
erikivanov7 <advictoriam@abv.bg>
2021-12-05 19:14:15 +0200

    Implemented login page functionality

--- 1-front-end-practice/1-react/4-online-books-library/client/src/components/Login/Login.js
+++ 1-front-end-practice/1-react/4-online-books-library/client/src/components/Login/Login.js
@@ -1,23 +1,56 @@
-const Login = () => {
+import useUser from '../../hooks/useUser.js';
+import { loginUser } from '../../services/userService.js';
+
+const Login = ({ history }) => {
+
+    const { addUser } = useUser();
+
+    const onLoginFormSubmit = async (ev) => {
+
+        ev.preventDefault();
+
+        const formData = new FormData(ev.target);
+
+        const email = formData.get('email').trim();
+
+        const password = formData.get('password').trim();
+
+        if (!email || !password) return alert('All fields are required!');
+
+        try {
+
+            const user = await loginUser({ email, password });
+
+            addUser(user);
+
+            history.push('/dashboard');
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    };
 
     return (
-        <section id="login-page" class="login">
-            <form id="login-form" action="" method="">
+        <section id="login-page" className="login">
+            <form id="login-form" onSubmit={onLoginFormSubmit}>
                 <fieldset>
                     <legend>Login Form</legend>
-                    <p class="field">
-                        <label for="email">Email</label>
-                        <span class="input">
+                    <p className="field">
+                        <label htmlFor="email">Email</label>
+                        <span className="input">
                             <input type="text" name="email" id="email" placeholder="Email" />
                         </span>
                     </p>
-                    <p class="field">
-                        <label for="password">Password</label>
-                        <span class="input">
+                    <p className="field">
+                        <label htmlFor="password">Password</label>
+                        <span className="input">
                             <input type="password" name="password" id="password" placeholder="Password" />
                         </span>
                     </p>
-                    <input class="button submit" type="submit" value="Login" />
+                    <input className="button submit" type="submit" value="Login" />
                 </fieldset>
             </form>
         </section>
