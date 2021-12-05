196a998990c9cf259d97ebc1d6e1248770fc554f
erikivanov7 <advictoriam@abv.bg>
2021-12-05 19:00:37 +0200

    Changed class to className in create comp

--- 1-front-end-practice/1-react/4-online-books-library/client/src/components/Create/Create.js
+++ 1-front-end-practice/1-react/4-online-books-library/client/src/components/Create/Create.js
@@ -1,31 +1,31 @@
 const Create = () => {
 
     return (
-        <section id="create-page" class="create">
+        <section id="create-page" className="create">
             <form id="create-form" action="" method="">
                 <fieldset>
                     <legend>Add new Book</legend>
-                    <p class="field">
+                    <p className="field">
                         <label for="title">Title</label>
-                        <span class="input">
+                        <span className="input">
                             <input type="text" name="title" id="title" placeholder="Title" />
                         </span>
                     </p>
-                    <p class="field">
+                    <p className="field">
                         <label for="description">Description</label>
-                        <span class="input">
+                        <span className="input">
                             <textarea name="description" id="description" placeholder="Description"></textarea>
                         </span>
                     </p>
-                    <p class="field">
+                    <p className="field">
                         <label for="image">Image</label>
-                        <span class="input">
+                        <span className="input">
                             <input type="text" name="imageUrl" id="image" placeholder="Image" />
                         </span>
                     </p>
-                    <p class="field">
+                    <p className="field">
                         <label for="type">Type</label>
-                        <span class="input">
+                        <span className="input">
                             <select id="type" name="type">
                                 <option value="Fiction">Fiction</option>
                                 <option value="Romance">Romance</option>
