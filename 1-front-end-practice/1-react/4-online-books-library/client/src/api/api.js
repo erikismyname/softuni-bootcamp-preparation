79fd9a6bababf85fbe3195979856064b1694e574
erikivanov7 <advictoriam@abv.bg>
2021-12-05 17:27:39 +0200

    Added an abstract http request func

---
+++ 1-front-end-practice/1-react/4-online-books-library/client/src/api/api.js
@@ -0,0 +1,25 @@
+const request = async (url, options) => {
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+
+        const error = await response.json();
+
+        throw new Error(error.message);
+
+    }
+
+    try {
+
+        const data = await response.json();
+
+        return data;
+
+    } catch (err) {
+
+        return response;
+
+    }
+
+};
