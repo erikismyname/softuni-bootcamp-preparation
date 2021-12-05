c010caa930fb96a26fd59d6b4477273601c8fd2d
erikivanov7 <advictoriam@abv.bg>
2021-12-05 17:43:30 +0200

    Created a user service

---
+++ 1-front-end-practice/1-react/4-online-books-library/client/src/services/userService.js
@@ -0,0 +1,13 @@
+import { ENDPOINTS } from '../config/constants.js';
+import { getRequest, postRequest } from '../api/api.js';
+
+const loginUser = async (userData) =>
+    postRequest(`${ENDPOINTS.user}/login`, null, userData);
+
+const logoutUser = async (token) =>
+    getRequest(`${ENDPOINTS.user}/logout`, token);
+
+const registerUser = async (userData) =>
+    postRequest(`${ENDPOINTS.user}/register`, null, userData);
+
+export { loginUser, logoutUser, registerUser };
